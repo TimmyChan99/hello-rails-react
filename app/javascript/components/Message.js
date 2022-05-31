c2cfe211daad50101b6ffea8b7d5e0c46255946a
TimmyChan99 <felemenoun@gmail.com>
2022-05-31 21:44:35 +0100

    fix linters errors

--- app/javascript/components/Message.js
+++ app/javascript/components/Message.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react"
 import { useDispatch, useSelector } from 'react-redux';
-import PropTypes from "prop-types"
 import { getMessagesAPI } from "../configureStore";
 
 const Message = () =>  {
